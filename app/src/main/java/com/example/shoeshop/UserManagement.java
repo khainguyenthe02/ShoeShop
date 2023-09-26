2eaa45d462468dbbd5bbe95ca0459501d9eec796
nguyenThekhai02 <khaid@MSI>
2023-09-26 11:26:22 +0700

    first commit

---
+++ app/src/main/java/com/example/shoeshop/UserManagement.java
@@ -0,0 +1,15 @@
+package com.example.shoeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_management);
+
+    }
+}
